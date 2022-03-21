6d6bfea841a60ddf1375dd713ed61c2dcb6335c8
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2022-03-22 03:12:33 +0530

    finalpush

---
+++ Day-11-Assignment-File_uploads/src/controllers/updateUser.controller.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = require("..");
+
+const User = require("../models/user.model");
+
+const uploads = require("../midlewares/uploads");
+
+const router = express.Router();
+
+router.post("", uploads.single("profilePic"), async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate({
+      profilePic: req.file.path,
+    });
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(404).send({ message: error.message });
+  }
+});
+
+module.exports = router;
